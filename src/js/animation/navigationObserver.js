bd5983d73f830d95ab28e5eafbd7c3b843dd73df
Timur <Timsonffs@gmail.com>
2022-05-23 18:23:11 +0600

    added hide content classes to global wrapper

---
+++ src/js/animation/navigationObserver.js
@@ -0,0 +1,34 @@
+export default function initObserver() {
+	const firstBlockObserver = document.querySelector('.intro-block-frame');
+	const navContent = document.querySelector('.nav-box');
+  const navBackground = document.querySelector('.nav-frame');
+	const menuLinkCollection = document.querySelectorAll('.nav-link');
+	const navLogo = document.querySelector('.nav-logo');
+
+	let options = {
+		threshold: 0.9,
+		rootMargin: '0px',
+	};
+
+	const observer = new IntersectionObserver(function (entries) {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting) {
+        navBackground.classList.add('nav-frame-scrolled');
+				navContent.classList.add('nav-box-scrolled');
+				navLogo.classList.add('nav-logo-scrolled');
+				menuLinkCollection.forEach((element) => {
+					element.classList.add('nav-link-scrolled');
+				});
+			} else {
+        navBackground.classList.remove('nav-frame-scrolled');
+				navContent.classList.remove('nav-box-scrolled');
+				navLogo.classList.remove('nav-logo-scrolled');
+				menuLinkCollection.forEach((element) => {
+					element.classList.remove('nav-link-scrolled');
+				});
+			}
+		});
+	}, options);
+
+	observer.observe(firstBlockObserver);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,28 @@
 import changeTitleColor from './animation/animations.js';
-import initObserver from './navigation/navigationObserver.js';
+import initObserver from './animation/navigationObserver.js';
+import isMobileDevice from './helpers/checkMobile.js';
+import AOS from 'aos/dist/aos.js'
 
-const content = document.querySelector('.hideContentWhileLoading');
+const content = document.querySelector('.initialContent');
 const animationBox = document.querySelector('.title-frame')
 const spinner = document.querySelector('.spinnerBackground');
 const body = document.querySelector('body');
 body.style.overflowY = 'hidden';
-animationBox.classList.add('animationOnloadDelay')
+content.classList.add('hideContentWhileLoading');
+animationBox.classList.add('animationOnloadDelay');
 
-window.onload = function() {
+function contentReady() {
+
   spinner.remove();
   body.style.overflowY = 'auto';
   content.classList.remove('hideContentWhileLoading');
+  content.classList.add('showContentWhenLoaded');
   animationBox.classList.remove('animationOnloadDelay');
-  if (document.documentElement.clientWidth > 770) {
-    initObserver();
-  }
 
-  changeTitleColor();
+  if (!isMobileDevice()) initObserver();
+  else body.tabIndex = 0; // чтобы работал кривой IOS c button: hover эфектами при табе.
 
   AOS.init({
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1500, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
@@ -28,6 +30,9 @@ window.onload = function() {
     anchorPlacement: 'bottom-bottom', // defines which position of the element regarding to window should trigge
     disable: 'mobile',
   });
+
+  changeTitleColor();
+
 }
 
-
+window.onload = contentReady;
