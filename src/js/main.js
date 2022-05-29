a1ea75b0e55bbfbf1c9c1caa2ef960756f145a05
Timur <Timsonffs@gmail.com>
2022-05-29 18:10:01 +0600

    fixed animation bug for lesser devices

--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,7 @@ function contentReady() {
     duration: 1500, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
     once: true, // whether animation should happen only once - while scrolling down
-    anchorPlacement: 'bottom-bottom', // defines which position of the element regarding to window should trigge
+    anchorPlacement: 'bottom-bottom',
     disable: 'mobile',
   });
 
