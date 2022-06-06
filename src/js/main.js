a2321ca677d1986e65786651f9035b213d15fd0f
Timur <Timsonffs@gmail.com>
2022-06-06 23:35:48 +0600

    replaced intro block images in right side animation frame

--- src/js/main.js
+++ src/js/main.js
@@ -3,36 +3,41 @@ import initObserver from './animation/navigationObserver.js';
 import isMobileDevice from './helpers/checkMobile.js';
 import AOS from 'aos/dist/aos.js'
 
-const content = document.querySelector('.loadingContentWrapper');
-const animationBox = document.querySelector('.title-frame')
-const spinner = document.querySelector('.spinnerBackground');
-const body = document.querySelector('body');
-body.style.overflowY = 'hidden';
-content.classList.add('hideContentWhileLoading');
-animationBox.classList.add('animationOnloadDelay');
-
-function contentReady() {
-
-  spinner.remove();
-  body.style.overflowY = 'auto';
-  content.classList.remove('hideContentWhileLoading');
-  content.classList.add('showContentWhenLoaded');
-  animationBox.classList.remove('animationOnloadDelay');
-
-  if (isMobileDevice()) body.tabIndex = 0; // чтобы работал кривой IOS c button: hover эфектами при табе.
-  else initObserver();
-
-  AOS.init({
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 1500, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: true, // whether animation should happen only once - while scrolling down
-    anchorPlacement: 'bottom-bottom',
-    disable: 'mobile',
-  });
-
-  changeTitleColor();
-
+if (isMobileDevice()) {
+  document.querySelector('.spinnerBackground').remove();
+  document.body.tabIndex = 0;
+  AOS.init({ disable: 'mobile' });
 }
+else {
+  const content = document.querySelector('.loadingContentWrapper');
+  const animationBox = document.querySelector('.title-frame')
+  const spinner = document.querySelector('.spinnerBackground');
+  const body = document.querySelector('body');
+  spinner.style.display = 'flex';
+  body.style.overflowY = 'hidden';
+  content.classList.add('hideContentWhileLoading');
+  animationBox.classList.add('animationOnloadDelay');
 
-window.onload = contentReady;
+  function contentReady() {
+
+    spinner.style.display = 'none';
+    body.style.overflowY = 'auto';
+    content.classList.remove('hideContentWhileLoading');
+    content.classList.add('showContentWhenLoaded');
+    animationBox.classList.remove('animationOnloadDelay');
+
+    initObserver();
+
+    AOS.init({
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 1500, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: true, // whether animation should happen only once - while scrolling down
+      anchorPlacement: 'bottom-bottom',
+      disable: 'mobile',
+    });
+
+    changeTitleColor();
+  }
+  window.onload = contentReady;
+}
